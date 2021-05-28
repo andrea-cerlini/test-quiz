ea7048213fd0aa244c0306b94a4de44c57aa2825
andrea-cerlini <andreacerlini2003@gmail.com>
2021-05-28 16:49:10 +0200

    fixato ciclo per il calcolo del best di un giocatore

--- script.js
+++ script.js
@@ -123,11 +123,11 @@ function updateDatabase() {
     console.log("Aggiornamento database");
     var found = false;
     var playerName = document.getElementsByClassName("name").item(0).value;
-    for (player in playerDatabase) { // Scorro la lista dei giocatori nel database per vedere se già c'è
-        if (playerName == player.name) { // Guardo tra i nomi se c'è quello del giocatore corrente
+    for (var i = 0; i < playerDatabase.length; i++) { // Scorro la lista dei giocatori nel database per vedere se già c'è
+        if (playerName == playerDatabase[i].name) { // Guardo tra i nomi se c'è quello del giocatore corrente
             console.log("Giocatore esistente!");
-            if (score > player.best) { // Se il giocatore è presente, vedo se il suo score è maggiore del best
-                player.bestScore = score;
+            if (score > playerDatabase[i].best) { // Se il giocatore è presente, vedo se il suo score è maggiore del best
+                playerDatabase[i].bestScore = score;
             }
             found = true;
         }
